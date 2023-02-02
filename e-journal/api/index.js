16bdd832fe7a573b3751bd9d03f36acc518c0731
ekfman <ekfirstman@gmail.com>
2023-02-02 09:08:59 -0800

    api index file

---
+++ e-journal/api/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const entriesRouter = require("./entries");
+router.use("/entries", entriesRouter);
+
+router.get("/health", (req, res, next) => {
+    res.send({
+      healthy: true,
+    });
+  });
+
+module.exports = router;
