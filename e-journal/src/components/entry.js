508a7c52f95906ef113d3ddb2a391791a9984bba
ekfman <ekfirstman@gmail.com>
2023-02-04 14:55:40 -0800

    created Entry component

---
+++ e-journal/src/components/entry.js
@@ -0,0 +1,17 @@
+
+
+const Entry = () => {
+    
+    
+    return (
+        <>
+            <form>
+                <input defaultValue="Title"></input>
+                <input defaultValue="Descrpition"></input>
+                <input defaultValue="Get all your thought out here."></input>
+            </form>
+        </>
+    )
+}
+
+export default Entry;
