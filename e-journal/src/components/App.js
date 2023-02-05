39f416f1b694cb4dcf480f830790a4e3db4f2f56
ekfman <ekfirstman@gmail.com>
2023-02-05 11:59:09 -0800

    created entry state

--- e-journal/src/components/App.js
+++ e-journal/src/components/App.js
@@ -1,89 +1,113 @@
-import './App.css';
+import "./App.css";
 // import { useState } from 'react';
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
-import format from 'date-fns/format';
-import parse from 'date-fns/parse';
-import startOfWeek from 'date-fns/startOfWeek';
-import getDay from 'date-fns/getDay';
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { useNavigate} from 'react-router-dom';
-import Entry from './Entry';
-import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import Entry from "./Entry";
+import { useState } from "react";
 // import { MonthView } from 'react-calendar';
 // import DatePicker from "react-datepicker";
 // import settings from '/assets'
 
 const locales = {
-  "en-US": require("date-fns/locale/en-US")
-}
+  "en-US": require("date-fns/locale/en-US"),
+};
 
 const localizer = dateFnsLocalizer({
   format,
   parse,
   startOfWeek,
   getDay,
-  locales
-})
+  locales,
+});
 
 const entries = [
-  {title: "New Dog",
-  description: "Got a new puppy",
-  content: "today I got a labradoodle. Her name is Sally",
-  start: new Date('2023','02','03'),
-  end: new Date('2023','02','03')
-  }
-]
+  {
+    title: "New Dog",
+    description: "Got a new puppy",
+    content: "today I got a labradoodle. Her name is Sally",
+    start: new Date("2023", "02", "03"),
+    end: new Date("2023", "02", "03"),
+  },
+];
 
 function App() {
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate();
+
   const [createEntry, setCreateEntry] = useState(false);
+  const [date, setDate] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
   const newEntryHandler = () => {
     navigate("/entry");
-    setCreateEntry( prev => !prev);
-  }
+    setCreateEntry((prev) => !prev);
+  };
 
   const handleEntryClick = () => {
-    console.log("clicked!")
-  }
+    console.log("clicked!");
+  };
 
   const handleSelectDate = () => {
     console.log("create a journal entry for today!");
-  }
- return (
-  <div className="app">
-    <nav>
-      <div className="dropdown">
-    <button className="dropbtn">Customize
-      <i className="fa fa-caret-down"></i>
-    </button>
-    <div className="dropdown-content">
-      <button>Dreamy</button>
-      <button>Sleek</button>
-      <button>Clean</button>
+  };
+  return (
+    <div className="app">
+      <nav>
+        <div className="dropdown">
+          <button className="dropbtn">
+            Customize
+            <i className="fa fa-caret-down"></i>
+          </button>
+          <div className="dropdown-content">
+            <button>Dreamy</button>
+            <button>Sleek</button>
+            <button>Clean</button>
+          </div>
+        </div>
+      </nav>
+      {createEntry ? (
+        <>
+          <Entry
+            navigate={navigate}
+            createEntry={createEntry}
+            setCreateEntry={setCreateEntry}
+            date={date}
+            setDate={setDate}
+            title={title}
+            setTitle={setTitle}
+            content={content}
+            setContent={setContent}
+          />
+        </>
+      ) : (
+        <>
+          <button className="newEntryButton" onClick={newEntryHandler}>
+            Create Entry
+          </button>
+          <div className="calendar-container">
+            <center>
+              <Calendar
+                localizer={localizer}
+                style={{ height: 500, margin: "50px" }}
+                views={["month", "day"]}
+                events={entries}
+                startAccessor="start"
+                endAccessor="end"
+                onSelectEvent={handleEntryClick}
+                onSelectSlot={handleSelectDate}
+              />
+            </center>
+            <div className="text-center"></div>
+          </div>
+        </>
+      )}
     </div>
-  </div>
-    </nav>
-    { createEntry ? (
-       <>
-       <Entry navigate={navigate} createEntry={createEntry} setCreateEntry={setCreateEntry}/>
-   </>
-    ) : (
-      <>
-    <button className='newEntryButton' onClick={newEntryHandler}>Create Entry</button>
-    <div className="calendar-container">
-      <center><Calendar localizer={localizer} style={{height: 500, margin: "50px"}} views={['month', 'day']} events={entries} 
-        startAccessor='start' endAccessor='end' onSelectEvent={handleEntryClick}
-        onSelectSlot={handleSelectDate}/></center>
-    <div className="text-center">
-    </div>
-    </div>
-      </>
-    ) }
-  </div>
-   )
- 
- }
+  );
+}
 
 export default App;
--- e-journal/src/components/entry.js
+++ e-journal/src/components/entry.js
@@ -1,25 +1,32 @@
 
 
-const Entry = ( {navigate, createEntry, setCreateEntry }) => {
+const Entry = ( {navigate, createEntry, setCreateEntry, date, setDate, title, setTitle, content, setContent }) => {
     const cancelHandler = () => {
         setCreateEntry( prev => !prev)
         navigate("/")
     }
-    const onSubmit = (e) => {
+    const onSubmit =  async (e) => {
         e.preventDefault();
         console.log('clicked ');
         const formData = new FormData(e.target)
         console.log('formData :>> ', formData);
+        try {
+            
+        } catch (error) {
+            
+        }
     }
+    console.log('title :>> ', title);
+    console.log('date :>> ', date);
 
     return (
         <>
             <form className="form-container" onSubmit={onSubmit}>
-                <input className="date" name="date" type="date"/>
+                <input className="date" type="date" onChange={e => setDate(e.target.value)}/>
                 <br></br>
-                <input className="title" name="title" placeholder="Title"></input>
+                <input className="title" placeholder="Title" onChange={e => setTitle(e.target.value)}></input>
                 <br></br>
-                <textarea className="content" name="content" rows="5" cols="60" type="text" placeholder="What happened?! Tell me."></textarea>
+                <textarea className="content" rows="5" cols="60" type="text" placeholder="What happened?! Tell me." onChange={e => setContent(e.target.value)}></textarea>
             </form>
             <button onClick={cancelHandler}>Cancel</button>
             <button type="submit">Submit Entry</button>
